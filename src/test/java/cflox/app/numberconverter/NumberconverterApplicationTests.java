ab6281354acbc73540cc3afc79fa8ef09ab8220e
Anand Advani <anandtrust08@gmail.com>
2023-12-09 11:24:00 +0100

    initial project commit

---
+++ src/test/java/cflox/app/numberconverter/NumberconverterApplicationTests.java
@@ -0,0 +1,13 @@
+package cflox.app.numberconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumberconverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
